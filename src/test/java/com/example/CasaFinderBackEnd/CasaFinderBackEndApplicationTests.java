13f9728762540dd1100c2fa6e5373b64b480813a
annunziatadr99 <anudar99@hotmail.it>
2025-03-23 23:03:51 +0100

    Initial commit

---
+++ src/test/java/com/example/CasaFinderBackEnd/CasaFinderBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CasaFinderBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaFinderBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
